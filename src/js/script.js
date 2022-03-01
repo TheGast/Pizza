d17456b2648330fc227e76c4aef2b597b226153c
TheGast <wotthegast@yandex.ru>
2022-03-01 13:23:50 +0300

    фильтр

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@ const CitySelect = document.querySelector(".header__city");
 const CityList = document.querySelector(".header__city-list");
 const CurrentCity = document.querySelector(".header__current-city");
 const CityItems = document.querySelectorAll(".header__city-item");
+const FilterButton = document.querySelector(".filter-btn");
 
 
 
@@ -24,5 +25,26 @@ CitySelect.addEventListener("click", function(){
 CityItems.forEach(function(item){
 	item.addEventListener("click", function(){
 		CurrentCity.innerHTML=item.textContent;
+	})
+})
+
+// Фильтры
+
+FilterButton.addEventListener("click", function(){
+	const Filter = document.querySelector(".filter");
+	const FilterWrapper = document.querySelector(".filter__wrapper");
+	const BtnClose = document.querySelector(".filter__btn-close");
+
+	DocumentBody.classList.add("hidden-on");
+	Filter.classList.add("bluer-on");
+
+	document.addEventListener("click", function(event){
+		if(event.target != Filter && event.target != BtnClose){
+			event.stopPropagation();
+		}
+		else{
+			DocumentBody.classList.remove("hidden-on");
+			Filter.classList.remove("bluer-on");
+		}
 	})
 })
